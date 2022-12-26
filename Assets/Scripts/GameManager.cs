24ede9d6d2db175c06f8313d2490b790db992932
syl3n7 <claudiosilva@protonmail.com>
2022-12-26 18:46:15 +0000

    Imported all figma assets
    https://github.com/syl3n7/MicroMacro-2D/issues/1
    Solved :+1:  :100:

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,22 +7,40 @@ public class GameManager : MonoBehaviour
 
     public Button dealButton;
     public Player player, dealer;
+    
+    private float lastWidth;
+    private float lastHeight;
 
 
     void Start()
     {
         GameObject.Find("Deck").GetComponent<Deck>().Shuffle();
         dealButton.onClick.AddListener(() => Deal());
+        Screen.SetResolution(1080, 1920, false, 60);
+        
     }
 
     void Update()
     {
-        
+
+        SetRatio(9, 16);
+
     }
 
     private void Deal()
     {
         player.GetHand();
         dealer.GetHand();
+    }
+    void SetRatio(float w, float h) //function to set the ratio of the screen to maintain the aspect of the resolution asked by the teacher (1080x1920)
+    {
+        if ((((float)Screen.width) / ((float)Screen.height)) > w / h)
+        {
+            Screen.SetResolution((int)(((float)Screen.height) * (w / h)), Screen.height, false);
+        }
+        else
+        {
+            Screen.SetResolution(Screen.width, (int)(((float)Screen.width) * (h / w)), false);
+        }
     }
 }
