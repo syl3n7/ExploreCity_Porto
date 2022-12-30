34f71995e859b35515333f6c0337f174d9c567db
syl3n7 <claudiosilva@protonmail.com>
2022-12-30 21:30:54 +0000

    Saving Current Changes
    Co-Authored-By: Mariana041 <100358775+Mariana041@users.noreply.github.com>

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,19 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = Unity.Mathematics.Random;
 
 public class Buttons : MonoBehaviour
 {
+    
+    public TMP_Text pname;
+    public TMP_Text region;
+    public TMP_Text money;
+    public TMP_Text tickets;
+    public TMP_Text level;
+    public TMP_Text rank;
+    public TMP_Text xp;
+    public TMP_Text gamesplayed;
 
+    RangeInt r = new RangeInt();
+    
+    void Start()
+    {
+        string currentScene = SceneManager.GetActiveScene().ToString();
+        if(currentScene == "MainMenu")
+        {
+            ReadUserInfo();
+        }
+
+        if (currentScene == "LoginScreen")
+        {
+            if (PlayerPrefs.GetString("PlayerName") != "" && PlayerPrefs.GetString("PlayerRegion") != "")
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+    }
     public void LoadScene(string sCene)
     {
-        SceneManager.LoadScene(sCene);
+        if (sCene == "CreateAvatar")
+        {
+            GenerateSaveGuest();
+            SceneManager.LoadScene(sCene);
+        }
+        else
+        {
+            SceneManager.LoadScene(sCene);
+        }
     }
 
+    public void ReadUserInfo()
+    {
+        pname.text = PlayerPrefs.GetString("PlayerName");
+        region.name = PlayerPrefs.GetString("PlayerRegion");
+    }
+    
+    public void SavePlayerNameRegion()
+    {
+        PlayerPrefs.SetString("PlayerName", pname.text);
+        PlayerPrefs.SetString("PlayerRegion", region.text);
+        Debug.Log(PlayerPrefs.GetString("PlayerName")+"\n"+PlayerPrefs.GetString("PlayerRegion"));
+        PlayerPrefs.Save();
+    }
+
+    public void GenerateSaveGuest()
+    {
+        PlayerPrefs.SetString("PlayerName", "Player0"+r.ToString());
+        PlayerPrefs.SetString("PlayerRegion", "WorldWide");
+        Debug.Log(PlayerPrefs.GetString("PlayerName")+"\n"+PlayerPrefs.GetString("PlayerRegion"));
+        PlayerPrefs.Save();
+    }
+    
     public void ExitGame()
     {
         Application.Quit();
     }
-    
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,36 +2,21 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Buttons
 {
-    /*
-    public Button dealButton;
-    public Player player, dealer;
-    
     private float lastWidth;
     private float lastHeight;
 
-
     void Start()
     {
-        GameObject.Find("Deck").GetComponent<Deck>().Shuffle();
-        dealButton.onClick.AddListener(() => Deal());
         Screen.SetResolution(1080, 1920, false, 60);
-        
     }
 
     void Update()
     {
-
         SetRatio(9, 16);
-
     }
-
-    private void Deal()
-    {
-        player.GetHand();
-        dealer.GetHand();
-    }
+    
     void SetRatio(float w, float h) //function to set the ratio of the screen to maintain the aspect of the resolution asked by the teacher (1080x1920)
     {
         if ((((float)Screen.width) / ((float)Screen.height)) > w / h)
@@ -43,5 +28,4 @@ public class GameManager : MonoBehaviour
             Screen.SetResolution(Screen.width, (int)(((float)Screen.width) * (h / w)), false);
         }
     }
-    */
 }
