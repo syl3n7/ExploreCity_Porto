d336d408410b1f6347e3da83ec6fb2562039d614
syl3n7 <claudiosilva@protonmail.com>
2023-01-14 16:53:20 +0000

    Big update
    Merges Scenes
    we now have only 3 scenes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,13 +9,25 @@ public class Player
     int tickets = 0;
     string name = null;
     string region = null;
-
+    protected Sprite hat;
+    protected Sprite head;
+    protected Sprite eyes;
+    protected Sprite mouth;
+    protected Sprite legs;
+    protected Sprite feet;
+    
     Player(/*int money, int tickets, */ string name, string region)
     {
         money = 0;
         tickets = 5; //default 5 tickets at begginig of game
         this.name = name;
         this.region = region;
+        hat = Resources.Load<Sprite>("Logos/github.png");
+        head = Resources.Load<Sprite>("Logos/github.png");
+        eyes = Resources.Load<Sprite>("Logos/github.png");
+        mouth = Resources.Load<Sprite>("Logos/github.png");
+        legs = Resources.Load<Sprite>("Logos/github.png");
+        feet = Resources.Load<Sprite>("Logos/github.png");
     }
 
     public int AddMoney(int moneyToAdd)
