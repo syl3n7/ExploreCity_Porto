9d64f89c6e0428e052f8dbbefbb4ee4d030de100
Mariana041 <a041337@ipmaia.pt>
2023-01-21 22:28:32 +0000

    Colocação de interação botões de voltar ao menu
    Script Quit Game
    Colocação de interação botões de voltar ao menu

---
+++ Assets/Scripts/Quiit Manager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuiitManager : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
