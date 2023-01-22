fa941a2edc6d3f13073613657899cbeed09e1f5e
syl3n7 <a040516@ipmaia.pt>
2023-01-22 08:15:55 +0000

    Final Commit before holidays

--- Assets/Scripts/ChangeName.cs
+++ Assets/Scripts/ChangeName.cs
@@ -5,42 +5,33 @@ using UnityEngine.UI;
 
 public class ChangeName : MonoBehaviour
 {
-    public Button button, button2;
-    public string[] Names = { "Portugal", "Espanha", "França", "Suiça", "Dinamarca", "Eslovénia", "Itália", "Worldwide" };
+    public string[] Names = { "Portugal", "Espanha", "França", "Suiça", "Dinamarca", "Eslovénia", "Itália", "Worldwide"};
     public string[] Names2 = { "Português", "Español", "François", "Svenska", "Danks", "Italiano" };
-    public TMPro.TMP_Text country, language;
+    public TMPro.TMP_Text country, language, country2;
     int i = 0;
     int z = 0;
 
-    void Start()
+    public void changeName(int x)
     {
-        button.onClick.AddListener(() => changeName());
-        button2.onClick.AddListener(() => changeName2());
-    }
-
-    public void changeName()
-    {
-        if (i < Names2.Length)
+		if (i >= 0 && i < Names.Length)
         {
             country.text = Names[i];
-        }else if (i == Names.Length) i = 0;  
-        else
-        {
-            Debug.Log("Can't find the name in the index" + i + " " + Names[i]);
+			country2.text = Names[i];
+			i = i + (x);
+			if(i == Names.Length) i = 0;
+			if(i == -1) i = 0;
         }
-        i++;
     }
 
-    public void changeName2()
+    public void changeName2(int x)
     {
         if (z < Names2.Length)
         {
-            language.text = Names2[z];
-        }else if (z == Names2.Length) z = 0;  
-        else
-        {
-            Debug.Log("Can't find the name in the index" + z + " " + Names2[z]);
+            language.text = Names2[z];        
+			z = z + (x);
+ 			if (z == Names2.Length) z = 0;
+			if (z == -1) z = 0;
         }
-        z++;
+		
     }
 }
